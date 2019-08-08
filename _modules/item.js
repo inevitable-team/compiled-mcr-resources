ece53ff854d78c98f6ed20c06eb46b9712255650
Sean O'Mahoney <seandavidomahoney@gmail.com>
2019-08-08 15:29:52 +0100

    Style Tweaks

--- _modules/item.js
+++ _modules/item.js
@@ -1,6 +1,6 @@
 module.exports = (imgSrc, title, role, url, desc) => `
 <div class="item">
-    <div class="itemImgContainer"><img src="${imgSrc}" alt="${title + " Profile Picture" }"></div>
+    <div class="itemImgContainer"><img src="${imgSrc}" alt="${title + " Profile Picture" }" loading="lazy"></div>
     <div class="itemContent">
         <h3>${title}</h3>
         <p class="itemShort">${ role ? (role + " | ") : "" }<a href="${ url }" target="_blank" rel="noopener noreferrer">${ url }</a></p>
