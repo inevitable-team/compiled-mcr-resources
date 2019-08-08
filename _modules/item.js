c6897b7d01fea94bb231c48ed54ed26a221f647c
Sean O'Mahoney <seandavidomahoney@gmail.com>
2019-08-08 21:17:16 +0100

    Item Role in Item Tags (Highlighted Red)

--- _modules/item.js
+++ _modules/item.js
@@ -3,8 +3,8 @@ module.exports = (imgSrc, title, tags, role, url, desc) => `
     <div class="itemImgContainer"><img src="${imgSrc}" alt="${title + " Profile Picture" }" loading="lazy"></div>
     <div class="itemContent">
         <h3>${title}</h3>
-        <div class="itemTags">${tags.map(tag => "<p>" + tag + "</p>").join("\n")}</div>
-        <p class="itemShort">${ role ? (role + " | ") : "" }<a href="${ url }" target="_blank" rel="noopener noreferrer">${ url }</a></p>
+        <div class="itemTags">${[role ? "<p class='role'>" + role + "</p>" : "", ...tags.map(tag => "<p>" + tag + "</p>")].join("\n")}</div>
+        <p class="itemShort"><a href="${ url }" target="_blank" rel="noopener noreferrer">${ url }</a></p>
         <p class="itemDesc">${desc}</p>
     </div>
 </div>
